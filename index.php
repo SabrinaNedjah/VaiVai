c1e7308b9a875aaf611ed5c4904e56a219bce70b
kerbrat <louis_kerbrat@hotmail.fr>
2017-02-02 17:28:06 +0100

    ajout des texte

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php include('partials/_header.php'); ?>
-<?php include('partials/_nav.php'); ?>
-<?php include("partials/_slider.php"); ?>
-<section class="jeuxConcours">
+    <?php include('partials/_nav.php'); ?>
+        <?php include("partials/_slider.php"); ?>
+            <section class="jeuxConcours">
 
-	<section class="section-intro">
-		<div class="row">
-			<div class="small-10 small-centered">
-				<h4 class="tag">Jeux Concours</h4>
-				<p class="description">Retrouvez Vaïvaï sur vos réseaux préférés et plongez avec nous dans notre univers complètement coco !</p>
-			</div>
-		</div>
-	</section>
-</section>
+                <section class="section-intro">
+                    <div class="row">
+                        <div class="small-10 small-centered">
+                            <h4 class="tag">Jeux Concours</h4>
+                            <p class="description">Retrouvez Vaïvaï sur vos réseaux préférés et plongez avec nous dans notre univers complètement coco !</p>
+                            <div>Vaïvaï est une petite entreprise française créée par deux amis qui vend des produits à base de noix de coco, principalement de l’eau de coco. Afin de répandre l’esprit coco, Vaïvaï organise un grand concours tout au long du Coconut Tour ! En partageant vos moments sur les réseaux sociaux avec le hashtage #Taspaslescocos vous pourrez tenter de gagner une séance dédicaces avec Gaël Monfils, des produits Vaïvaï et bien plus encore !</div>
+                        </div>
+                    </div>
+                </section>
+            </section>
 
-<div class="contener">
- <h2 class="titreContener">Coconut TV</h2>
- <div class="textContener"> ekjerkejrkerj</div>
-	 <a href="coconutTv.php" class="bouton">En Savoir plus</a>
-</div>
+            <div class="contener">
+                <h2 class="titreContener">Coconut TV</h2>
+                <div class="textContener"> ekjerkejrkerj</div>
+                <a href="coconutTv.php" class="bouton">En Savoir plus</a>
+            </div>
 
-<div class="contener">
+            <div class="contener">
 
- <h2 class="titreContener">Je suis à DROITE</h2>
- <div class="textContener"> BLABLABLA</div>
+                <h2 class="titreContener">Je suis à DROITE</h2>
+                <div class="textContener"> BLABLABLA</div>
 
-	 <a href="#" class="bouton">En Savoir plus</a>
-</div>
+                <a href="#" class="bouton">En Savoir plus</a>
+            </div>
 
 
 
-<?php include("partials/_footer.php"); ?>
-
+            <?php include("partials/_footer.php"); ?>
